f99739bf77e7a780e871e9bc5c68192b193400aa
Mannuel <mannuel.ferreira@gmail.com>
2013-04-15 15:36:54 +0200

    Hello World
    1st steps

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Joao ");
+
+            Console.WriteLine("Hi ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Your talk yesterday lit a fire under my arse to learn " + str);
+            
+
+            /*
+            int mySeconds = 60 * 60 * 24 * 365;
+            Console.WriteLine("There are {0} seconds in a year", mySeconds);
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
